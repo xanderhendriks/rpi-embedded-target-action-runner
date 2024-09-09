4d62a95da9383e9e679da19f67169f09e180cce7
Xander Hendriks <xander@nx-solutions.com>
2024-09-09 12:49:32 +1000

    Added unit test step to the pipeline and fixed the application cppcheck issues for lab 7

--- application/Source/crc.c
+++ application/Source/crc.c
@@ -9,14 +9,15 @@ static const uint16_t Crc_Table[16] = {0x0000, 0x1021, 0x2042, 0x3063, 0x4084, 0
 
 uint16_t Crc_Calculate16Bit(uint16_t crc, const uint8_t *data, uint32_t length)
 {
-    uint16_t i;
-    uint16_t shiftedData;
     uint32_t lenIterator;
     uint16_t tempCrc = crc;
-    uint32_t tempCrc32;
 
     for (lenIterator = length; lenIterator > 0U; lenIterator--)
     {
+        uint16_t i;
+        uint16_t shiftedData;
+        uint32_t tempCrc32;
+
         shiftedData = ((uint16_t)(*data) >> 4);
         i           = (tempCrc >> 12) ^ shiftedData;
         tempCrc32   = (uint32_t) tempCrc << 4;
